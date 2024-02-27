519c614170fbeadb24b1458774ffdc980789daf8
TheWrongAlice <alice.cyan.carlsson@gmail.com>
2024-02-27 22:03:05 +0100

    Tidy up migrations

--- db/migrate/110_create_portraits.rb
+++ db/migrate/110_create_portraits.rb
@@ -3,10 +3,10 @@
 class CreatePortraits < ActiveRecord::Migration[7.0]
   def change
     create_table :portraits do |t|
-      t.column :name, :string
-      t.column :species, :string, array: true, default: []
-      t.column :genders, :string, array: true, default: []
-      t.column :groups, :string, array: true, default: []
+      t.string :name
+      t.string :species, array: true, default: []
+      t.string :genders, array: true, default: []
+      t.string :groups, array: true, default: []
       t.timestamps
     end
   end
--- lib/custom_cops/custom_validator_name.rb
+++ lib/custom_cops/custom_validator_name.rb
@@ -2,38 +2,40 @@
 
 module RuboCop
   module Cop
-    # Ensure custom validators to have prefix "must_"
-    class UsePrefixForCustomValidator < RuboCop::Cop::Cop
-      MSG = 'Add a prefix "must_" to custom validator names'
+    module Custom
+      # Ensure custom validators to have prefix "must_"
+      class UsePrefixForCustomValidator < RuboCop::Cop::Cop
+        MSG = 'Add a prefix "must_" to custom validator names'
 
-      CUSTOM_VALIDATOR_METHOD = :validate
+        CUSTOM_VALIDATOR_METHOD = :validate
 
-      def on_send(node)
-        method = method_name(node)
+        def on_send(node)
+          method = method_name(node)
 
-        return unless custom_validator_method?(method)
+          return unless custom_validator_method?(method)
 
-        first_argument_node = first_argument(node)
+          first_argument_node = first_argument(node)
 
-        add_offense(first_argument_node, location: :expression) if missing_prefix?(first_argument_node.value)
-      end
+          add_offense(first_argument_node, location: :expression) if missing_prefix?(first_argument_node.value)
+        end
 
-      private
+        private
 
-      def custom_validator_method?(method)
-        method == CUSTOM_VALIDATOR_METHOD
-      end
+        def custom_validator_method?(method)
+          method == CUSTOM_VALIDATOR_METHOD
+        end
 
-      def method_name(node)
-        node.children[1]
-      end
+        def method_name(node)
+          node.children[1]
+        end
 
-      def first_argument(node)
-        node.arguments[0]
-      end
+        def first_argument(node)
+          node.arguments[0]
+        end
 
-      def missing_prefix?(custom_validator_name)
-        !custom_validator_name.to_s.include?('must_')
+        def missing_prefix?(custom_validator_name)
+          custom_validator_name.to_s.exclude?('must_')
+        end
       end
     end
   end
