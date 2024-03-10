7f0815b6c977952fc8768c63ef54cdfbb6b5b937
TheWrongAlice <alice.cyan.carlsson@gmail.com>
2024-03-10 10:49:46 +0100

    Move sidekiq schedule to yml file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -16,25 +16,3 @@ Sidekiq.configure_server do |config|
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
   end
 end
-
-# Define job schedule
-# Useful cron expression generator: https://crontab.cronhub.io/
-# WARNING: Changing the names of jobs may result in duplicate cron schedules. Schedules can be disabled and removed in the Sidekiq Web UI.
-Sidekiq::Cron::Job.create(
-  name: 'Create new dungeons',
-  cron: '0/2 * * * *', # Every 2 minutes
-  class: 'DungeonCreateWorker',
-  timezone: 'UTC',
-)
-Sidekiq::Cron::Job.create(
-  name: 'Schedule dungeon expirations',
-  cron: '1/2 * * * *', # Every 2 minutes, starting at 1 minutes past the hour (to avoid conflict with 'Create new dungeons')
-  class: 'DungeonExpirationScheduler',
-  timezone: 'UTC',
-)
-Sidekiq::Cron::Job.create(
-  name: 'Destroy expired dungeons',
-  cron: '0 * * * *', # Every hour
-  class: 'DungeonDestroyWorker',
-  timezone: 'UTC',
-)
