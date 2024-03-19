6c02fcab7f6c4ecd6785babacce9d97f2cf8b292
TheWrongAlice <alice.cyan.carlsson@gmail.com>
2024-03-19 21:23:38 +0100

    include monster in dungeon serializer

--- app/serializers/dungeon_serializer.rb
+++ app/serializers/dungeon_serializer.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class DungeonSerializer < RealmLocationSerializer
-  attributes :defeated_by
+  attributes :defeated_by, :monster
 
   def defeated_by
     ActiveModelSerializers::SerializableResource.new(object.defeated_by, each_serializer: SafeUserSerializer)
+  end
+
+  def monster
+    ActiveModelSerializers::SerializableResource.new(object.monster, each_serializer: MonsterSerializer)
   end
 end
