ffab4408bf224d6b55a9746bfa192ae0e39d483a
TheWrongAlice <alice.cyan.carlsson@gmail.com>
2024-04-27 21:33:46 +0200

    Improved testing for DateTimeHelper

--- test/lib/date_time_helper_test.rb
+++ test/lib/date_time_helper_test.rb
@@ -3,6 +3,16 @@
 require 'test_helper'
 
 class DateTimeHelperTest < ActiveSupport::TestCase
+  setup do
+    # Define a stub for Timezone.lookup to return a mock object with a name method
+    Timezone.define_singleton_method(:lookup) do |lat, lon|
+      OpenStruct.new(name: 'Europe/Oslo')
+    end
+  end
+  teardown do
+    # Restore the original Timezone.lookup method after each test
+    Timezone.singleton_class.remove_method(:lookup)
+  end
   test 'returns correct timezone' do
     oslo_latitude = 59.907887
     oslo_longitude = 10.751164
