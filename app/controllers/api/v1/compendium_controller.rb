5b5754d07fe91ea91637f066aea57cf7637a8b90
TheWrongAlice <alice.cyan.carlsson@gmail.com>
2024-08-22 15:22:44 +0200

    Disallow compendium endpoints in PROD

--- app/controllers/api/v1/compendium_controller.rb
+++ app/controllers/api/v1/compendium_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Api::V1::CompendiumController < ApplicationController
+  before_action :env_guard
+
   def monsters
     @monsters = Monster.all
     render json: @monsters, each_serializer: MonsterSerializer, compendium: true
@@ -9,5 +11,11 @@ class Api::V1::CompendiumController < ApplicationController
   def items
     @items = Item.all
     render json: @items, each_serializer: ItemSerializer, compendium: true
+  end
+
+  private
+
+  def env_guard
+    render status: :method_not_allowed if Rails.env.production?
   end
 end
